6c6ee788d78066c7fa503845749dae0a5a8c4297
Erick <erickpichon11@gmail.com>
2023-07-22 16:10:57 -0600

    Ya puedo compilar sass, y tambien escucha por los cambios

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp =require("gulp"),
+sass=require('gulp-sass')(require('sass')),
+autoprefixer=require("gulp-autoprefixer");
+
+gulp.task("sass",function(){
+    return gulp.src("sass/scss/*.scss")
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest("sass/css/"))
+});
+
+gulp.task("default",function(){
+    return gulp.watch("sass/scss/*.scss",gulp.series("sass"));
+});
+
