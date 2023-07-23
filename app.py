bf220b6e1ee05314e8135430a56394883dacce9f
Erick <erickpichon11@gmail.com>
2023-07-22 20:14:39 -0600

    Tenia el problema de que no reconocia mis estilos

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template
 
-app=Flask(__name__)
+app = Flask(__name__, static_folder='public') #//Con esta le digo donde estara mi carpeta con archivos estaticos
 
 @app.route("/")
 def inicio():
--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ autoprefixer=require("gulp-autoprefixer");
 gulp.task("sass",function(){
     return gulp.src("sass/scss/*.scss")
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest("sass/css/"))
+    .pipe(gulp.dest("public/css"))
 });
 
 gulp.task("default",function(){
